9aaf13a8d2b8f4e895b8eb4be31c912d314e2962
Mr. Yap <15719191+mryap@users.noreply.github.com>
2022-11-07 09:24:57 +0800

    Update Footer.js

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -5,6 +5,7 @@ import logo from "../img/logo.svg";
 import facebook from "../img/social/facebook.svg";
 import instagram from "../img/social/instagram.svg";
 import twitter from "../img/social/twitter.svg";
+import vimeo from "../img/social/vimeo.svg";
 
 const Footer = () => {
   
