d5fec15fb75d30172892716add019191a5545b02
Mr. Yap <15719191+mryap@users.noreply.github.com>
2022-11-07 09:40:28 +0800

    Update Footer.js

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -5,7 +5,7 @@ import logo from "../img/logo.svg";
 import facebook from "../img/social/facebook.svg";
 import instagram from "../img/social/instagram.svg";
 import twitter from "../img/social/twitter.svg";
-//import vimeo from "../img/social/vimeo.svg";
+
 
 const Footer = () => {
   
@@ -93,13 +93,6 @@ const Footer = () => {
                   <img
                     src={instagram}
                     alt="Instagram"
-                    style={{ width: "1em", height: "1em" }}
-                  />
-                </a>
-                <a title="vimeo" href="https://vimeo.com">
-                  <img
-                    src={vimeo}
-                    alt="Vimeo"
                     style={{ width: "1em", height: "1em" }}
                   />
                 </a>
